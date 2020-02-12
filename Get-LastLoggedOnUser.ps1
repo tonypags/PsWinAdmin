fedb056a8464f20d3e43954b9b4df6be3e7b0b00
Tony Pagliaro <tpagliaro@rfa.com>
2020-02-11 20:50:13 -0500

    updated logic with renamed function.

--- Get-LastLoggedOnUser.ps1
+++ Get-LastLoggedOnUser.ps1
@@ -2,10 +2,10 @@ function Get-LastLoggedOnUser {
     Param(
     
     )
-        $Win32Users  = Get-Win32Users
+        $Win32Users  = Get-Win32User
         $LastUser = $Win32Users | Sort-Object -Property LastUseTime -Descending | Select-Object -First 1
         $UserSID = New-Object System.Security.Principal.SecurityIdentifier($LastUser.SID)
         
-        Convert-UserSID -SID $UserSID.Value
+        Get-LocalUserAccount -Sid $UserSID.Value
     
 }
