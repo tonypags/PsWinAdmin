e92ffbced40e06827c99a44886b5e4b00945dabf
Tony Pagliaro <tony@pagliaro.co>
2020-03-04 18:01:33 -0500

    Used .Net to get DC list instead of ADSI or netdom

--- Get-DomainControllers.ps1
+++ Get-DomainControllers.ps1
@@ -8,21 +8,17 @@ function Get-DomainControllers {
     Get-DomainControllers
     DC1
     DC2
-    .NOTES
-    netdom command is for servers only.
-    This is OK for this command for now, but future usage may require it work on Workstations. 
-    Build a new version of this function that will leverage ADSI commands.
     #>
 
     Try {
 
-        $DCs = & netdom query dc
-        $DCs[2..($DCs.count -3)]
+        [DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain(
+        ).DomainControllers.Name -replace '\..*'
 
     } Catch {
 
         Write-Warning "Could not enumerate DC list."
-        Write-Warning "Make sure this device is a domain controller."
+        Write-Warning "Make sure this device is a domain-joined computer or server."
         break
 
     }
