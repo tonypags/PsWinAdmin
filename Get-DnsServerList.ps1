57597b4e72e363e6df344f17b0d54708841e720d
Tony Pagliaro <Tony@Pagliaro.co>
2021-07-07 14:50:57 -0400

    Added more output data, DNS resolve and test steps

--- Get-DnsServerList.ps1
+++ Get-DnsServerList.ps1
@@ -1,3 +1,4 @@
+
 function Get-DnsServerList {
     <#
     .SYNOPSIS
@@ -5,7 +6,7 @@ function Get-DnsServerList {
     .DESCRIPTION
     Leverages the Get-DnsClientServerAddress cmdlet and returns
     device info including ServerName, InterfaceAlias, and
-    an ordered list of ServerAddresses. WinRM/CIM access using
+    an ordered list of IP Addresses. WinRM/CIM access using
     $Credential is required for non-local queries.
     .PARAMETER ComputerName
     Pull DNS info from this target(s). Default is localhost.
@@ -16,25 +17,27 @@ function Get-DnsServerList {
     .EXAMPLE
     Get the local device's DNS info.
 
-    PS > Get-DnsServerList -AddressFamily IPv4,IPv6
+    PS > Get-DnsServerList -AddressFamily IPv4,IPv6 |
+    >> Select ServerName, InterfaceAlias, DnsIpStack
 
-    ServerName InterfaceAlias ServerAddresses
+    ServerName InterfaceAlias DnsIpStack
     ---------- -------------- ---------------
     PC2        Ethernet       {10.10.2.12, 4.2.2.2}
     .EXAMPLE
     Get the local device's IPv4 DNS info.
 
-    PS > Get-DnsServerList
+    PS > Get-DnsServerList | Select ServerName, InterfaceAlias, DnsIpStack
 
-    ServerName InterfaceAlias ServerAddresses
+    ServerName InterfaceAlias DnsIpStack
     ---------- -------------- ---------------
     PC1        Ethernet       {10.10.1.12, 1.1.1.1}
     .EXAMPLE
     Get a remote device's IPv4 DNS info.
 
-    PS > Get-DnsServerList PC2 -Credential (Get-Credential)
+    PS > Get-DnsServerList PC2 -Credential (Get-Credential) |
+    >> Select ServerName, InterfaceAlias, DnsIpStack
 
-    ServerName InterfaceAlias ServerAddresses
+    ServerName InterfaceAlias DnsIpStack
     ---------- -------------- ---------------
     PC2        Ethernet       {10.10.2.12, 4.2.2.2}
     .EXAMPLE
@@ -42,9 +45,9 @@ function Get-DnsServerList {
 
     PS > $sv = Get-ADSIComputerInfo -OsType 'Windows Server'
     PS > $rpt = Get-DnsServerList -ComputerName ($sv.Computer)
-    PS > $rpt[48,99]
+    PS > $rpt[48,99] | Select ServerName, InterfaceAlias, DnsIpStack
 
-    ServerName  InterfaceAlias ServerAddresses
+    ServerName  InterfaceAlias DnsIpStack
     ----------  -------------- ---------------
     PRDVWSWHA02 Ethernet0      {10.202.102.150, 10.6.1.55, 10.207.100.150}
     halas       Ethernet       {10.202.102.150, 10.6.1.55, 10.207.100.150}
@@ -76,7 +79,18 @@ function Get-DnsServerList {
                 $_.CimSystemProperties.ServerName
             }}
             'InterfaceAlias'
-            'ServerAddresses'
+            @{Name='DnsIpStack';Exp={
+                $_.ServerAddresses
+            }}
+            @{Name='DnsIpList';Exp={
+                $_.ServerAddresses -join ', '
+            }}
+            @{Name='ServerFqdn';Exp={
+                $thisDNS.Name
+            }}
+            @{Name='ServerIp';Exp={
+                $thisDNS.IPAddress
+            }}
 
         )
 
@@ -99,33 +113,43 @@ function Get-DnsServerList {
                 # do nothing
             } else {
 
-                Try {
-                
-                    $cimProps = @{
-                        ComputerName = $Computer
-                        Credential = $Credential
-                        ErrorAction = 'Stop'
-                    }
-                    $CimSession = New-CimSession @cimProps
+                $thisDNS = (Resolve-DnsName halas -Type A -NoHostsFile).Where(
+                    {$_ -is [Microsoft.DnsClient.Commands.DnsRecord_A]}
+                ).Where(
+                    {$_.Name -like "$($Computer)*"}
+                )
 
-                    $dnsProps.Add('CimSession',$CimSession)
-                    $netProps.Add('CimSession',$CimSession)
-                    Write-Verbose "Connected to CimSession on $(
-                        $Computer
-                    )"
+                if (Test-Connection -ComputerName $thisDNS.IPAddress -Count 2 -Quiet) {
 
-                } Catch {
-                
-                    Write-Warning "Could not connect to $(
-                        $Computer
-                    ): $(
-                        $Error[0].Exception.Message
-                    )"
+                    Try {
                     
-                    # Skip check since no connection
-                    continue
-
-                }#END: Try {}
+                        $cimProps = @{
+                            ComputerName = $Computer
+                            Credential = $Credential
+                            ErrorAction = 'Stop'
+                        }
+                        $CimSession = New-CimSession @cimProps
+    
+                        $dnsProps.Add('CimSession',$CimSession)
+                        $netProps.Add('CimSession',$CimSession)
+                        Write-Verbose "Connected to CimSession on $(
+                            $Computer
+                        )"
+    
+                    } Catch {
+                    
+                        Write-Warning "Could not connect to $(
+                            $Computer
+                        ): $(
+                            $Error[0].Exception.Message
+                        )"
+                        
+                        # Skip check since no connection
+                        continue
+    
+                    }#END: Try {}
+                
+                }#END: if (Test-Connection -Computer...-Quiet) {}
 
             }#END: if ($Computer -eq $env:COMPUTERNAME) {}
 
