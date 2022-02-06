b701ac371d25bc488aa4404a5d30495048d23dd4
Tony Pagliaro <tony@pagliaro.co>
2022-02-05 23:19:22 -0500

    Add should process to iis pool function

--- Public/Reset-IisApplicationPool.ps1
+++ Public/Reset-IisApplicationPool.ps1
@@ -1,11 +1,22 @@
 function Reset-IisApplicationPool {
+    [CmdletBinding(
+        ConfirmImpact="High",
+        SupportsShouldProcess=$true
+    )]
     $WmiProps = @{
         namespace = "root\MicrosoftIISv2"
         class = "IIsApplicationPool"
     }
-    $appPools = Get-WmiObject @WmiProps |
-        Select-Object -ExpandProperty Name
-    foreach ($appPool in $AppPools) {
-       $appPool.Recycle()
+    $appPools = (Get-WmiObject @WmiProps).Name
+    
+    if ($pscmdlet.ShouldProcess(
+        "appPools",
+        "Recycle"
+    )) {
+        foreach ($appPool in $AppPools) {
+            # Should Process?
+            $appPool.Recycle()
+        }
     }
+
 }
