c5229ee2fe80d13b070226926320cb6141ef5650
Tony Pagliaro <tony@pagliaro.co>
2022-04-20 14:59:34 -0400

    Add help/examples and positions

--- Public/Test-Port.ps1
+++ Public/Test-Port.ps1
@@ -1,13 +1,25 @@
 function Test-Port {
+    <#
+    .SYNOPSIS
+    Allows port testing with variable timeout
+    .DESCRIPTION
+    Allows TCP port testing with variable timeout
+    .EXAMPLE
+    Test-Port Server1 443
+    .EXAMPLE
+    Test-Port -Computer Server1 -Port 443
+    .EXAMPLE
+    Test-Port -IPAddress Server1 -Port 443 -Timeout 250
+    #>
     [CmdletBinding()]
     [OutputType([bool])]
     param(
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory,Position=0)]
         [Alias('ComputerName')]
         [string]
         $IPAddress,
 
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory,Position=1)]
         [Alias('TcpPort')]
         [int]
         $Port,
