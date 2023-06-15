dddcc2a8a10ab0375506c4a1eb90d18be264869c
Tony Pagliaro <tony@pagliaro.co>
2023-06-15 15:20:31 -0400

    Add support for mapped drives

--- Public/Get-CimLocalDisk.ps1
+++ Public/Get-CimLocalDisk.ps1
@@ -9,7 +9,11 @@ function Get-CimLocalDisk
         # The target computer(s) to search for PST files.
         [Parameter()]
         [string[]]
-        $ComputerName = @($env:COMPUTERNAME)
+        $ComputerName = @($env:COMPUTERNAME),
+        
+        [Parameter()]
+        [ValidateSet('LocalDisk','DriveMap','Any')]
+        $Type = 'LocalDisk'
     )
 
     Foreach ($Computer in $ComputerName) {
@@ -17,9 +21,15 @@ function Get-CimLocalDisk
         $cimSplat = @{
             Namespace = 'root/cimv2'
             Class = 'win32_logicaldisk'
-            Filter = "DriveType='3'"
             ErrorAction = 'Stop'
         }    
+        
+        switch ($Type) {
+            'LocalDisk' {$cimSplat.Filter = "DriveType='3'"}
+            'DriveMap'  {$cimSplat.Filter = "DriveType='4'"}
+            'Any' {}
+            Default {Write-Warning 'Unhandled Drive Type! Returning Any...'}
+        }
 
         # Add computername if not this computer
         if (@($env:COMPUTERNAME,'localhost') -notcontains $Computer) {
