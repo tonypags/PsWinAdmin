316815eff163af64a57a5f14c56752e17c533e0d
Tony Pagliaro <tony@pagliaro.co>
2024-06-10 15:38:51 -0400

    Add support for stream-writing to file

--- Public/Write-Log.ps1
+++ Public/Write-Log.ps1
@@ -24,20 +24,35 @@ function Write-Log
     [string]$var | log @LogSplat 
     .EXAMPLE
     [string]$var | log @LogSplat -EntryType Error
+    .EXAMPLE
+    $FilePath = 'c:\temp\log.txt'
+    $streamWriter = New-Object System.IO.StreamWriter($filePath)
+    $LogSplat = @{
+        StreamWriter = $streamWriter
+        TimestampFormat = 'yyyy\-MM\-dd\_HH\:mm\:ss'
+        EntryType = 'INFO'
+    }
+    'Message here' | Write-Log @LogSplat 
+    $streamWriter.Close()
     .INPUTS
     The message contents to add to the log file can be piped 
     into the function as a single string value. 
     #>
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='Slow')]
     [Alias('log')]
     Param
     (
         # Location of the log file
-        [Parameter(Mandatory=$true,
-                  Position=0)]
+        [Parameter(ParameterSetName='Slow',
+                  Mandatory,Position=0)]
         [ValidatePattern('.+\.log|.+\.txt')]
         [string]
         $FilePath,
+
+        # $streamWriter = New-Object System.IO.StreamWriter($filePath) ; $streamWriter.Close()
+        [Parameter(ParameterSetName='Fast')]
+        [System.IO.StreamWriter]
+        $StreamWriter,
 
         # The message contents to add to the log file
         [Parameter(Mandatory=$true,
@@ -48,9 +63,10 @@ function Write-Log
         $Content,
 
        # Define the datestamp string, defaults to DiDictionary
-       [string]
+       [Parameter()]
        [ValidateNotNull()]
        [ValidateNotNullOrEmpty()]
+       [string]
        $TimestampFormat = 'yyyy-MM-dd_HH:mm:ss.fff',
 
        # Type of message, allowed are DEBUG, INFO*, WARN, ERROR, FAIL, etc
@@ -74,38 +90,34 @@ function Write-Log
         $EntryType = $EntryType.ToUpper()
         
         # Make sure the file exists
-        if(Test-Path $FilePath){}else{
-            New-Item -ItemType File -Path $FilePath -Force | 
-                Out-Null
-        }
-
-        # Find the local domain
-        $lclDomain = Try {
-            (Get-CimInstance Win32_ComputerSystem -ea Stop).Domain.ToUpper()
-        } Catch {
-            $env:USERDNSDOMAIN
+        if ($PSCmdlet.ParameterSetName -eq 'Fast') {$FilePath = $streamWriter.BaseStream.Name}
+        if(Test-Path $FilePath -ea 0){}else{
+            New-Item -ItemType File -Path $FilePath -Force | Out-Null
         }
 
         # Build the string, starting with the date and type
-        [string]$strContent = $null
-        $strContent = $strContent + "$((Get-Date).ToString($TimestampFormat)) "
-        $strContent = $strContent + "[$($EntryType)]: "
-        $strContent = $strContent + (whoami) + '@'
-        $strContent = $strContent + $env:COMPUTERNAME + '.'
-        $strContent = $strContent + $lclDomain + ' '
-        $strContent = $strContent + $Content
+        [string]$strContent = "$((Get-Date).ToString($TimestampFormat)) [$($EntryType)]: $(whoami)@$($env:COMPUTERNAME) $Content"
 
         # Add Content to the file.
         Try {
-            $Splat = @{
-                Value = $strContent
-                Path = $FilePath
-                Force = $true
+            $origEAP = $ErrorActionPreference
+            if ($PSCmdlet.ParameterSetName -eq 'Slow') {
+                $Splat = @{
+                    Value = $strContent
+                    Path = $FilePath
+                    Force = $true
+                }
+                Add-Content @Splat -ea Stop -ev LogError        # SLOW
+            } elseif ($PSCmdlet.ParameterSetName -eq 'Fast') {
+                $ErrorActionPreference = 'Stop'
+                $streamWriter.WriteLine($strContent)            # FAST
+                $ErrorActionPreference = $origEAP
+            } else {
+                throw "Unhandled ParameterSetName: $($PSCmdlet.ParameterSetName)"
             }
-            Add-Content @Splat -ea Stop -ev LogError
-
         } Catch {
-            
+            $ErrorActionPreference = $origEAP
+
             if (-not $Quiet -and -not $WriteLogErrorHappened) {
                 Write-Error "Logging error: $($LogError.ErrorRecord)`n"
                 Set-Variable -Name 'WriteLogErrorHappened' -Scope Global -Value $true # prevents multiple errors
